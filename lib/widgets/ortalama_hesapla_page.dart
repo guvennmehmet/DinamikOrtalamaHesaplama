248de86679cf7725363b141dc8074695f9293d9f
guvennmehmet <guvennmehmet37@gmail.com>
2023-02-04 14:19:02 +0300

    ana yapının oluşturulması, sabitler ve google fonts kullanımı

---
+++ lib/widgets/ortalama_hesapla_page.dart
@@ -0,0 +1,27 @@
+import 'package:dinamik_ortalama_hesaplama/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class OrtalamaHesaplaPage extends StatefulWidget {
+  const OrtalamaHesaplaPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrtalamaHesaplaPage> createState() => _OrtalamaHesaplaPageState();
+}
+
+class _OrtalamaHesaplaPageState extends State<OrtalamaHesaplaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Center(
+          child: Text(Sabitler.baslikText, style: Sabitler.baslikStyle,),
+        ),
+      ),
+      body: Center(
+        child: Text('Merhaba'),
+      ),
+    );
+  }
+}
